194e8544669cd279b439525dd4d8d0f3d034fa2c
yuin2018 <594150568@qq.com>
2018-10-06 13:26:39 +0800

    Site updated: 2018-10-06 13:26:37

--- js/my.js
+++ js/my.js
@@ -1,4 +1,6 @@
 if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
     alert("非法访问!")
-    window.close();
+    window.open("","_self"); 
+    window.close(); 
+    WeixinJSBridge.call('closeWindow');
 }
