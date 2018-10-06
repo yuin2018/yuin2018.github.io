68f2cf439909172c326b45bc66c9bc05d9d4ed7c
yuin2018 <594150568@qq.com>
2018-10-06 14:15:35 +0800

    Site updated: 2018-10-06 14:15:34

--- js/my.js
+++ js/my.js
@@ -1,9 +1,14 @@
 if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
-    alert("非法访问!")
-    if(typeof window.WeixinJSBridge != "undefined"){
-        alert(1)
-        WeixinJSBridge.call('closeWindow')
-    }
+    var div = document.createElement("div");
+    var text = document.createTextNode("非法访问!");
+    div.appendChild(text);
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    div.style.cssText =
+        'position: absolute;top:' + (windowHeight / 2) - 150 + 'px;left:' + (windowWidth / 2) - 250 / 2 +
+        'px;background: rgba(0,0,0,.8);border-radius: 10px;font-size: 14px;letter-spacing: 2px;margin: 0 auto;text-align: center;width: 250px;height: 100px;line-height: 100px;color: #fff;'
+    document.getElementsByTagName("body")[0].appendChild(div);
     window.location.href="about:blank";
     window.close();
+    WeixinJSBridge.call('closeWindow')
 }
