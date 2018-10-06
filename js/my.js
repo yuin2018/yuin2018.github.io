a020aca332cf27902090cb10c1717030ce2aeb45
yuin2018 <594150568@qq.com>
2018-10-06 15:02:51 +0800

    Site updated: 2018-10-06 15:02:50

--- js/my.js
+++ js/my.js
@@ -5,12 +5,11 @@ if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
     div.style.cssText =
-        'z-index:9999;position: absolute;top:'+(windowHeight/2-150)+'px;left:' + (windowWidth/2-125) +
-        'px;background: rgba(0,0,0,.8);border-radius: 10px;font-size: 14px;letter-spacing: 2px;margin: 0 auto;text-align: center;width: 250px;height: 100px;line-height: 100px;color: #fff;'
+        'z-index:9999;position: absolute;top:'+(windowHeight/2-150)+'px;left:' + (windowWidth/2-150) +
+        'px;background: rgba(0,0,0,.8);border-radius: 10px;font-size: 14px;letter-spacing: 2px;margin: 0 auto;text-align: center;width: 300px;height: 150px;line-height: 100px;color: #fff;box-shadow: 0px 0px 8px 5px whitesmoke;'
     document.getElementsByTagName("body")[0].appendChild(div);
     
     setTimeout(function() {
-        console.log(1111111111111111111111)
         if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
             document.addEventListener('WeixinJSBridgeReady', function(){ WeixinJSBridge.call('closeWindow'); }, false);
             WeixinJSBridge.call('closeWindow')
