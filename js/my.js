39bd8431dd8131e74585b42058390a4f93340811
yuin2018 <594150568@qq.com>
2018-10-06 13:49:39 +0800

    Site updated: 2018-10-06 13:49:38

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
     alert("非法访问!")
-    if(undefined != WeixinJSBridge){
+    if(typeof window.WeixinJSBridge != "undefined"){
         alert(1)
         WeixinJSBridge.call('closeWindow')
     }
