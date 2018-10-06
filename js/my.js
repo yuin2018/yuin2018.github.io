7cfa9d2ec07d8cbd4e4950c1c51e5c1cebe9ca5a
yuin2018 <594150568@qq.com>
2018-10-06 15:05:17 +0800

    Site updated: 2018-10-06 15:05:16

--- js/my.js
+++ js/my.js
@@ -6,7 +6,7 @@ if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
     var windowHeight = window.innerHeight;
     div.style.cssText =
         'z-index:9999;position: absolute;top:'+(windowHeight/2-150)+'px;left:' + (windowWidth/2-150) +
-        'px;background: rgba(0,0,0,.8);border-radius: 10px;font-size: 14px;letter-spacing: 2px;margin: 0 auto;text-align: center;width: 300px;height: 150px;line-height: 100px;color: #fff;box-shadow: 0px 0px 8px 5px whitesmoke;'
+        'px;background: rgba(0,0,0,.8);border-radius: 10px;font-size: 14px;letter-spacing: 2px;margin: 0 auto;text-align: center;width: 300px;height: 150px;line-height: 150px;color: #fff;box-shadow: 0px 0px 8px 3px whitesmoke;'
     document.getElementsByTagName("body")[0].appendChild(div);
     
     setTimeout(function() {
@@ -17,5 +17,5 @@ if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
             window.location.href="about:blank";
             window.close();
         }
-    }, 2000);
+    }, 1500);
 }
