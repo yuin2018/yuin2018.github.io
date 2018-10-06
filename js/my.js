b0f8be6175429fac31103289e2e17427d71df422
yuin2018 <594150568@qq.com>
2018-10-06 14:41:37 +0800

    Site updated: 2018-10-06 14:41:36

--- js/my.js
+++ js/my.js
@@ -5,17 +5,17 @@ if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
     div.style.cssText =
-        'z-index:9999;position: absolute;top:' + (windowHeight / 2) - 150 + 'px;left:' + (windowWidth / 2) - 250 / 2 +
+        'z-index:9999;position: absolute;top:'+(windowHeight/2-150)+'px;left:' + (windowWidth/2-125) +
         'px;background: rgba(0,0,0,.8);border-radius: 10px;font-size: 14px;letter-spacing: 2px;margin: 0 auto;text-align: center;width: 250px;height: 100px;line-height: 100px;color: #fff;'
     document.getElementsByTagName("body")[0].appendChild(div);
     
-    // setTimeout(function() {
-    //     if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
-    //         document.addEventListener('WeixinJSBridgeReady', function(){ WeixinJSBridge.call('closeWindow'); }, false);
-    //         WeixinJSBridge.call('closeWindow')
-    //     }else{
-    //         window.location.href="about:blank";
-    //         window.close();
-    //     }
-    // }, 2000);
+    setTimeout(function() {
+        if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
+            document.addEventListener('WeixinJSBridgeReady', function(){ WeixinJSBridge.call('closeWindow'); }, false);
+            WeixinJSBridge.call('closeWindow')
+        }else{
+            window.location.href="about:blank";
+            window.close();
+        }
+    }, 2000);
 }
