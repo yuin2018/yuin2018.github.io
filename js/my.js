e8b11fc3a633a28d8a7e643e64672a5f0c2b84a8
yuin2018 <594150568@qq.com>
2018-10-06 13:30:38 +0800

    Site updated: 2018-10-06 13:30:37

--- js/my.js
+++ js/my.js
@@ -1,6 +1,8 @@
 if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
     alert("非法访问!")
-    window.open("","_self"); 
-    window.close(); 
-    WeixinJSBridge.call('closeWindow');
+    // window.open("","_self"); 
+    // window.close(); 
+    window.location.href="about:blank";
+    window.close();
+    if(WeixinJSBridge)  WeixinJSBridge.call('closeWindow')
 }
