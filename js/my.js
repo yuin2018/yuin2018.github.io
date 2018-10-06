da06039eb1ba603bf5c574e5e759c4c8146f88f9
yuin2018 <594150568@qq.com>
2018-10-06 14:19:39 +0800

    Site updated: 2018-10-06 14:19:38

--- js/my.js
+++ js/my.js
@@ -5,10 +5,15 @@ if(document.referrer == "" && document.URL == "https://yuin2018.github.io/"){
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
     div.style.cssText =
-        'position: absolute;top:' + (windowHeight / 2) - 150 + 'px;left:' + (windowWidth / 2) - 250 / 2 +
+        'z-index:9999;position: absolute;top:' + (windowHeight / 2) - 150 + 'px;left:' + (windowWidth / 2) - 250 / 2 +
         'px;background: rgba(0,0,0,.8);border-radius: 10px;font-size: 14px;letter-spacing: 2px;margin: 0 auto;text-align: center;width: 250px;height: 100px;line-height: 100px;color: #fff;'
     document.getElementsByTagName("body")[0].appendChild(div);
-    window.location.href="about:blank";
-    window.close();
-    WeixinJSBridge.call('closeWindow')
+    setTimeout(() => {
+        if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
+            WeixinJSBridge.call('closeWindow')
+        }else{
+            window.location.href="about:blank";
+            window.close();
+        }
+    }, 333);
 }
