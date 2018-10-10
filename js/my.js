82042fc948dbd34155f98d2d1e08fcf5af5e4c25
yuin2018 <594150568@qq.com>
2018-10-10 22:46:26 +0800

    Site updated: 2018-10-10 22:46:22

--- js/my.js
+++ js/my.js
@@ -1,4 +1,5 @@
 if (document.referrer == "" && document.URL == "https://yuin2018.github.io/") {
+    document.getElementsByTagName("body")[0].style.overflow = 'hidden';
     var ua = navigator.userAgent.toLowerCase();
     var div = document.createElement("div");
     var text = document.createTextNode("非法访问!");
@@ -17,9 +18,11 @@ if (document.referrer == "" && document.URL == "https://yuin2018.github.io/") {
                     WeixinJSBridge.call('closeWindow');
                 }, false);
                 WeixinJSBridge.call('closeWindow');
-            }else if(ua.match(/mqqbrowser|qzone|qqbrowser/i)){
-                location.href = "http://google.com"
-            } else{
+            }
+            // else if(ua.match(/mqqbrowser|qzone|qqbrowser/i)){
+            //     location.href = "http://google.com"
+            // } 
+            else{
                 window.location.href = "about:blank";
                 window.close();
             }
@@ -29,3 +32,31 @@ if (document.referrer == "" && document.URL == "https://yuin2018.github.io/") {
         }
     }, 1500);
 }
+
+
+var yqc = {
+    name: '名字',
+    Hello: function(){
+        console.log('%c 很开心你能来看我','color: #fff;letter-space:1px;text-shadow: 0px 0px 1px #666;')
+    },
+    user:{
+        hello:function(){
+            console.log('欢迎')
+        }
+    },
+    log: function(msg){
+        console.log("%c"+ msg+"","color: #fff;letter-space:1px;text-shadow: 0px 0px 1px #666;")
+    }
+}; 
+yqc.Hello.prototype.english = function() {
+    yqc.log("hello");
+}
+yqc.Hello.prototype.chinese = function () {
+    yqc.log("你好");
+}
+window.onload = function(){
+    yqc.Hello();
+    // var e = new yqc.Hello();
+    // e.english()
+}
+
